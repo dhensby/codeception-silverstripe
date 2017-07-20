cc36283cb9a7a03816140d86f6a83f1f5c3019e7
Daniel Hensby <dhensby@silverstripe.com>
2017-07-20 17:13:03 +0100

    Correctly nest kernel, injector and config for test runs

--- src/Module/SilverStripe4.php
+++ src/Module/SilverStripe4.php
@@ -4,6 +4,9 @@ namespace Codeception\Module;
 
 use Codeception\Lib\Framework;
 use Codeception\Lib\Interfaces\ActiveRecord;
+use Codeception\TestInterface;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Dev\TestKernel;
 use SilverStripe\ORM\DataObject;
 
@@ -15,17 +18,66 @@ class SilverStripe4 extends Framework implements ActiveRecord
      */
     protected $kernel;
 
+    /**
+     * @var TestKernel[]
+     */
+    protected $kernelStack = [];
+
+    protected function nest()
+    {
+        if ($this->kernel) {
+            $this->kernel = $this->kernel->nest();
+            $this->kernelStack[] = $this->kernel;
+        } else {
+            $this->debug('Nothing to nest');
+        }
+        Injector::nest();
+        Config::nest();
+    }
+
+    protected function unnest()
+    {
+        if (!empty($this->kernelStack)) {
+            $this->kernel = array_pop($this->kernelStack);
+            $this->kernel->activate();
+        } else {
+            $this->debug('Nothing to unnest');
+        }
+        Injector::unnest();
+        Config::unnest();
+    }
+
     public function _initialize()
     {
         parent::_initialize();
         require_once FRAMEWORK_PATH . '/tests/bootstrap/environment.php';
+        $kernel = new TestKernel(BASE_PATH);
+        $kernel->boot();
+        $this->kernel = $kernel;
     }
 
     public function _beforeSuite($settings = [])
     {
         parent::_beforeSuite($settings);
-        $this->kernel = new TestKernel(BASE_PATH);
-        $this->kernel->boot();
+        $this->nest();
+    }
+
+    public function _afterSuite()
+    {
+        parent::_afterSuite();
+        $this->unnest();
+    }
+
+    public function _before(TestInterface $test)
+    {
+        parent::_before($test);
+        $this->nest();
+    }
+
+    public function _after(TestInterface $test)
+    {
+        parent::_after($test);
+        $this->unnest();
     }
 
     public function haveRecord($model, $attributes = [])
